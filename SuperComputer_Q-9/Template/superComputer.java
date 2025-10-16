aaa88f54a459a2fd4d9bb79176e210bc4a2b5908
Barter Fernando Perez Valdez <124309425+BarterFPV@users.noreply.github.com>
2025-10-16 01:30:02 -0500

    Add files via upload

---
+++ SuperComputer_Q-9/Template/superComputer.java
@@ -0,0 +1,32 @@
+package SuperComputer.Template;
+
+import java.util.Scanner;
+
+public class superComputer {
+
+    // TODO: Make a function that converts input str (Binary, Octal, Decimal, Hexadecimal) => Int (Decimal)
+    public static int toDecimal(String value) {
+        // TODO: Convert the input string to decimal
+        return 0; // Placeholder
+    }
+
+    public static void superChip(){
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: Take the input from the user
+        /*
+         * You can always assume the input will be a number 1 - 100 followed by the operations
+         * that will be in the form {value1_sign_value2} where _ is a space. v1, sign, and v2 could be
+         * in an invalid form.
+         */
+        
+        // TODO: Separate input to decimal
+        // TODO: Perform and print the given operation
+        
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        superChip();
+    }
+}
